20a297c16cba29d1fe73409f5593418f4a11f8ee
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-01 20:25:01 +0200

    Celsjusza On Fahrenheita

--- src/CelsjuszaOnFahrenheita.java
+++ src/CelsjuszaOnFahrenheita.java
@@ -6,8 +6,10 @@ public class CelsjuszaOnFahrenheita {
         Scanner stopnie = new Scanner(System.in);
         System.out.println("Witam, program służy do przeliczania stopni Celsjusza na Fahrenheita");
         System.out.println("Podaj stopnie Celcjusza: ");
+
         int Celcjusza = stopnie.nextInt();
         System.out.println("Stopnie Celcjusza wynoszą: " + Celcjusza);
+        
         double Fahrenheita = Celcjusza * 1.8 + 32;
         System.out.println("Po przeliczeniu stopnie Fahrenheitana wynoszą: " + Fahrenheita);
     }
